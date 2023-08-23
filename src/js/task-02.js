646cef5f1d648cd06b7b60f2f2f2a9d7d18b32a2
Konashuk <konan4ukv@gmail.com>
2023-08-23 12:52:20 +0300

    task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ulElem = document.querySelector("#ingredients");
+const murkup = ingredients.map((fruit) => {
+  const liElem = document.createElement("li");
+  const pElem = document.createElement("p");
+  pElem.textContent = fruit;
+  liElem.append(pElem);
+  liElem.classList.add("item");
+  return liElem;
+});
+ulElem.append(...murkup);
+console.log(ulElem);
