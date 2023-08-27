332c2f117804f0f7698bf90a5e3e44081502adaa
Konashuk <konan4ukv@gmail.com>
2023-08-27 18:35:42 +0300

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const formElem = document.querySelector(".login-form");
+console.dir(formElem);
+formElem.addEventListener("submit", submitInput);
 
+function submitInput(event) {
+  event.preventDefault();
+  const form = event.currentTarget.elements;
+  const email = form.email.value;
+  const password = form.password.value;
+  if (!email || !password) {
+    alert("All field need to check");
+  } else {
+    const formData = {
+      email,
+      password,
+    };
+    console.log(formData);
+  }
+  formElem.reset();
+}
