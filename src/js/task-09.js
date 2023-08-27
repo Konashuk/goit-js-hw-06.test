7d3e5ff50ae07314a929c25656dfb7cf4bc843fd
Konashuk <konan4ukv@gmail.com>
2023-08-27 18:51:09 +0300

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const containerEl = document.querySelector(".widget");
+const spanEl = document.querySelector(".color");
+
+containerEl.addEventListener("click", changeColorClick);
+
+function changeColorClick(event) {
+  document.body.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+}
