0721bc99ae23805ad16b58aec35c894d32a861c6
Konashuk <konan4ukv@gmail.com>
2023-08-27 20:09:07 +0300

    task 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,24 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const divEl = document.querySelector("#boxes");
+const createBtn = document.querySelector("button[data-create]");
+const inputDivEl = document.querySelector("#controls").firstElementChild;
+
+createBtn.addEventListener(`click`, valueInput);
+
+function createBoxes(amount) {
+  let createDiv = ``;
+  for (let i = 0; i < amount; i++) {
+    createDiv += `<div></div>`;
+  }
+  divEl.innerHTML = createDiv;
+}
+
+function valueInput(event) {
+  const valueInput = event.currentTarget.value;
+  createBoxes(valueInput);
+}
+console.log(divEl);
+console.log(createBtn);
+console.dir(inputDivEl);
